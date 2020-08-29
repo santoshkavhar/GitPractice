d1ccc96dcd68d752e49f6d46e6fbecf05ce99e10
santoshkavhar <santosh.kavhar@joshsoftware.com>
2020-08-29 14:29:42 +0530

    suntraction function added

--- calc.go
+++ calc.go
@@ -3,9 +3,14 @@ package main
 func main(){
 
 	fmt.Println(add(2,3))
+	fmt.Println(sub(2,3))
 
 }
 
 func add(num1, num2 int) int {
 	return num1 + num2
+}
+
+func sub(num1, num2 int) int{
+	return num1 - num2
 }
